c12e18aa4273cc45e90828073b3a60edccc1b7af
guredora <contact@guredora.com>
2020-06-13 05:53:42 +0900

    ページ数計算で切り上げするように修正。デバッグメッセージの修正。

--- Peing.py
+++ Peing.py
@@ -7,12 +7,12 @@ class Peing():
 		json = requests.get("	https://peing.net/api/v1/user/analysis_information/show?account=%s" % (self.id)).json()
 		return json
 	def get_user_answers(self, count):
-		pages = int(count/5)
-		print(pages)
+		pages = -(count*-1//5)
+		print("answer page count is %d", (pages))
 		answers = {}
 		for i in range(pages):
-			print(i)
-			json = requests.get("https://peing.net/api/v2/items/?type=answered&account=%s&page=%d" % (self.id, i)).json()
+			print("getting pages %d", (i+1))
+			json = requests.get("https://peing.net/api/v2/items/?type=answered&account=%s&page=%d" % (self.id, i+1)).json()
 			for item in json["items"]:
 				answers[item["body"]] = item["answer_body"]
 			time.sleep(0.5)
--- npc.py
+++ npc.py
@@ -7,10 +7,11 @@ argparse.add_argument("user")
 args = argparse.parse_args()
 peing = Peing.Peing(args.user)
 info = peing.get_user_info()
+print("item count is %d", (info["answers_count"]))
 answers = peing.get_user_answers(info["answers_count"])
 with open("%s_answers.csv" % (peing.id), "w", encoding="utf-8", newline="") as f:
 	answer_csv = csv.writer(f)
 	answer_csv.writerow(("質問", "回答"))
 	for q, a in answers.items():
 		answer_csv.writerow((q, a))
-
+print("Done!")
