a2ad15715ad28a70ac4e564971f002153032e81a
guredora <contact@guredora.com>
2020-06-20 19:41:14 +0900

    メッセージの%dがそのまま表示されていたのを修正

--- Peing.py
+++ Peing.py
@@ -8,10 +8,10 @@ class Peing():
 		return json
 	def get_user_answers(self, count):
 		pages = -(count*-1//5)
-		print("answer page count is %d", (pages))
+		print("answer page count is %d" % pages)
 		answers = {}
 		for i in range(pages):
-			print("getting pages %d", (i+1))
+			print("getting pages %d" % (i+1))
 			json = requests.get("https://peing.net/api/v2/items/?type=answered&account=%s&page=%d" % (self.id, i+1)).json()
 			for item in json["items"]:
 				answers[item["body"]] = item["answer_body"]
--- npc.py
+++ npc.py
@@ -1,3 +1,4 @@
+import winsound
 import argparse
 import Peing
 import csv
@@ -8,16 +9,16 @@ argparse.add_argument("user")
 args = argparse.parse_args()
 peing = Peing.Peing(args.user)
 info = peing.get_user_info()
-print("item count is %d", (info["answers_count"]))
+print("item count is %s" % (info["answers_count"]))
 answers = peing.get_user_answers(info["answers_count"])
-with open("%s_answers.csv" % (peing.id), "w", newline="") as f:
+with open("%s_answers.csv" % (peing.id), "wb") as f:
 	csvf = io.StringIO()
 	answer_csv = csv.writer(csvf)
 	answer_csv.writerow(("質問", "回答"))
 	for q, a in answers.items():
 		answer_csv.writerow((q, a))
-	text = csvf.getvalue().encode("CP932", "replace").decode("CP932")
-	print(len(text))
+	bin = csvf.getvalue().encode("CP932", "replace")
 	csvf.close()
-	f.write(text)
+	f.write(bin)
+winsound.Beep(1900, 300)
 print("Done!")
