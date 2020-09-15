42a78ee7525dfa97da5edaddbcdd140eec62447d
guredora <contact@guredora.com>
2020-09-16 00:09:16 +0900

    dbManagerでsqlite3が標準で対応していない型への変換を実装

--- dbManager.py
+++ dbManager.py
@@ -2,9 +2,24 @@ import sqlite3
 
 class dbManager():
 	def __init__(self, db_name):
-		self.connection = sqlite3.connect(db_name)
+		self.connection = sqlite3.connect(db_name, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
 		self.connection.row_factory = sqlite3.Row
+		sqlite3.dbapi2.converters["DATETIME"] = sqlite3.dbapi2.converters["TIMESTAMP"]
+		sqlite3.register_converter("BOOL", self._convert_boolean)
+		sqlite3.register_adapter(bool, self._adapt_boolean)
 		self.cursor = self.connection.cursor()
+
+	def _adapt_boolean(value):
+		if value:
+			return 1
+		if not value:
+			return 0
+
+	def _convert_boolean(value):
+		if int(value) == 0:
+			return False
+		if int(value) == 1:
+			return True
 
 	def select(self, *args):
 		self.cursor.execute(*args)
