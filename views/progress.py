340dab411fe8dd322f841bbb08b4623b353a7c2a
Kochi <c011610813@edu.teu.ac.jp>
2021-04-25 16:21:12 +0900

    プログレスバーなしでのProgressDialog利用に対応

--- views/progress.py
+++ views/progress.py
@@ -8,13 +8,16 @@ import views.ViewCreator
 
 from views.baseDialog import *
 
+MODE_NORMAL = 0		#テキスト１行・プログレスバー・ボタン
+MODE_NO_GAUGE = 1	#プログレスバーなし
 
 class Dialog(BaseDialog):
-	def __init__(self):
+	def __init__(self,mode=0):
 		super().__init__("gaugeDialog")
+		self.mode = mode
+		self._status = None
 
 	def Initialize(self, label, title):
-		self._status = None
 		self.label=label
 		self.log.debug("created")
 		super().Initialize(self.app.hMainView.hFrame,title)
@@ -24,14 +27,17 @@ class Dialog(BaseDialog):
 	def InstallControls(self):
 		"""いろんなwidgetを設置する。"""
 		self.creator=views.ViewCreator.ViewCreator(self.viewMode,self.panel,self.sizer,wx.VERTICAL,style=wx.ALL,space=20)
-		self.gauge,self.static=self.creator.gauge(self.label,x=400)
+		if self.mode&MODE_NO_GAUGE==0:
+			self.gauge,self.static=self.creator.gauge(self.label,x=500)
+		else:
+			self.static=self.creator.staticText(self.label)
 		self.button = self.creator.button(_("中止(&C)"), self.cancelEvent,sizerFlag=wx.ALIGN_CENTER)
 
 	# プログレス更新（現在値, ラベル, 最大値）
 	def update(self, pos=None, label=None, max=None):
-		if max != None:
+		if max != None and self.mode&MODE_NO_GAUGE==0:
 			self.gauge.SetRange(max)
-		if pos != None:
+		if pos != None and self.mode&MODE_NO_GAUGE==0:
 			self.gauge.SetValue(pos)
 		if label != None:
 			self.static.SetLabel(label)
